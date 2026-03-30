097a8b618720546cf47fa9bb61cde79ad336953b
szondamartin <112873676+szondamartin@users.noreply.github.com>
2026-03-30 16:22:34 +0200

    6. beadando 2026.03.30

---
+++ 5-6bead/app.js
@@ -0,0 +1,17 @@
+const GH_ACCESS_TOKEN = 'asdasd';
+
+// === Navbar betöltése ===
+function loadNavigation() {
+    fetch('./navbar.html')
+        .then(res => res.text())
+        .then(navbarHtml => {
+            document.body.insertAdjacentHTML('afterbegin', navbarHtml);
+        })
+        .catch(err => {
+            console.error(err);
+            alert('Hiba a menürendszer betöltésekor.');
+        });
+}
+
+loadNavigation();
+
